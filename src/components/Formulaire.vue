27959f12c18e14e4eb868500230b7c0acb4ade32
christian <delormechrist@gmail.com>
2021-04-16 17:38:35 +0200

    add form

--- src/components/Formulaire.vue
+++ src/components/Formulaire.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- <h1>{{ msg }}</h1> -->
-  <h1>inscriptions</h1>
+
   <div class="hello">
     <div class="card" style="width: 30rem; height: 15rem">
       <div class="card-body">
@@ -45,40 +45,125 @@
           <div class="line" style="width: rem; height: 3px"></div>
           <div class="line" style="width: rem; height: 3px"></div>
         </div>
+        <!-- ----------------------------------------- -->
         <div class="need_link">
-          <div class="need_link1">
-            <router-link to="/Place"></router-link>
-          </div>
-          <div class="need_link2">
-            <router-link to="/Material"></router-link>
-          </div>
-          <div class="need_link3">
-            <router-link to="/Worker"></router-link>
-          </div>
+          <form class="row g-3 needs-validation" novalidate>
+            <div class="col-md-4">
+              <label for="validationCustom01" class="form-label"
+                >First name</label
+              >
+              <input
+                type="text"
+                class="form-control"
+                id="validationCustom01"
+                required
+              />
+              <div class="valid-feedback">Looks good!</div>
+            </div>
+            <div class="col-md-4">
+              <label for="validationCustom02" class="form-label"
+                >Last name</label
+              >
+              <input
+                type="text"
+                class="form-control"
+                id="validationCustom02"
+                required
+              />
+              <div class="valid-feedback">Looks good!</div>
+            </div>
+
+            <!-- <div class="col-md-6">
+              <label for="validationCustomUsername" class="form-label"
+                >Username</label
+              >
+              <div class="input-group has-validation">
+                <span class="input-group-text" id="inputGroupPrepend">@</span>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="validationCustomUsername"
+                  placeholder="@"
+                  aria-describedby="inputGroupPrepend"
+                  required
+                />
+                <div class="invalid-feedback">Please choose a username.</div>
+              </div>
+            </div> -->
+
+<div  class="col-md-6">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+
+            <div class="col-md-6">
+              <label for="validationCustom03" class="form-label"
+                >enterprise</label
+              >
+              <input
+                type="text"
+                class="form-control"
+                id="validationCustom03"
+                required
+              />
+              <div class="invalid-feedback">Please choose an enterprise.</div>
+            </div>
+
+            <!-- <div class="col-md-3">
+    <label for="validationCustom04" class="form-label">State</label>
+    <select class="form-select" id="validationCustom04" required>
+      <option selected disabled value="">Choose...</option>
+      <option>...</option>
+    </select>
+    <div class="invalid-feedback">
+      Please select a valid state.
+    </div>
+  </div> -->
+
+            <!-- <div class="col-md-3">
+    <label for="validationCustom05" class="form-label">Zip</label>
+    <input type="text" class="form-control" id="validationCustom05" required>
+    <div class="invalid-feedback">
+      Please provide a valid zip.
+    </div>
+  </div> -->
+            <!-- <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+      <label class="form-check-label" for="invalidCheck">
+        Agree to terms and conditions
+      </label>
+      <div class="invalid-feedback">
+        You must agree before submitting.
+      </div>
+    </div>
+  </div> -->
+            <div class="col-12">
+              <button class="btn btn-primary" type="submit">Submit form</button>
+            </div>
+          </form>
         </div>
+        <!-- ------------------------------------------------ -->
       </div>
     </div>
   </div>
   <!-- <h4>{{ msg }}</h4> -->
-  <h4 class="dots">sign-up </h4>
-
- 
+  <h4 class="dots">sign-up</h4>
 </template>
 
 <script>
 export default {
   name: "Formulaire",
-//   props: {
-//     msg: String,
-//   },
+  //   props: {
+  //     msg: String,
+  //   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 
 <style scoped lang="scss">
-// -----[cadre acceuil]---------------------------------
-
+// -----[cadre]---------------------------------
 
 .card {
   position: relative;
@@ -137,7 +222,6 @@ h4 {
   display: inline-block;
   animation: dotty steps(1, end) 2s infinite;
   content: "";
-  
 }
 
 @keyframes dotty {
@@ -157,7 +241,8 @@ h4 {
     content: "";
   }
 }
-// ==============================================
+
+// =====[Formulaire]==================================
 
 .hello {
   display: flex;
@@ -166,5 +251,24 @@ h4 {
   height: 800px;
   justify-content: center;
 }
-
+.form-control {
+  color: #57e597;
+  background-color: black;
+  background-clip: padding-box;
+  border: 4px solid #57e597;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  border-radius: 0rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  text-shadow: #55e094 1px 0 10px;
+}
+label.form-label {
+  color: #57e597;
+}
+form.row.g-3.needs-validation {
+  display: flex;
+  justify-content: center;
+}
+// ====================================================
 </style>
